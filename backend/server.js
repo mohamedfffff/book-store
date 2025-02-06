4afec3cfad4c99b2a4f5f321fc2eae001e2ca93a
the thing <65288778+mohamedfffff@users.noreply.github.com>
2025-02-06 21:01:46 +0200

    moving package.json

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use("/books", router);
 
 //production configuration to run the frontend with the backend
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   });
